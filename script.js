55055bd096bd08b67a97c7c689a3a093eef3cf0c
blackdiamand <kevikeviwang@gmail.com>
2020-12-04 17:38:47 -0800

    work

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 var password = document.getElementById("password");
 var output = document.getElementById("output");
+var 
 
 password.oninput = function(event) {
     console.log(password.value)
   if(password.value.equals("buh")){
       output.innerHTML = "pog";
   }
+}
+
+document.getElementById("submit").addEventListener("click", pog);
+function pog(){    
+        if(document.getElementById("pass").value == ("buh")){
+        document.getElementById("output").innerHTML = "Success!";
+            showFiles();
+        }
+        else{
+            alert("Password incorrect");
+        }
+    }
+function showFiles(){
+    
+    
 }

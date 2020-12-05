f72f59e2286007f1e6787b0a81f37a0dffb20c5a
blackdiamand <kevikeviwang@gmail.com>
2020-12-04 17:54:03 -0800

    js link

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 var password = document.getElementById("password");
 var output = document.getElementById("output");
- 
-
-password.oninput = function(event) {
-    console.log(password.value)
-  if(password.value.equals("buh")){
-      output.innerHTML = "pog";
-  }
-}
+ console.log("work")
 
 document.getElementById("submit").addEventListener("click", pog);
 function pog(){    

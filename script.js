4aed003e8229cf412a50f8e8e4d9fdbbc1c5d1b7
blackdiamand <kevikeviwang@gmail.com>
2020-12-04 18:08:11 -0800

    buh work

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var password = document.getElementById("pass");
 var output = document.getElementById("output");
  console.log("work")
 
-document.getElementById("sub").addEventListener("click", pog);
+//document.getElementById("sub").addEventListener("click", pog);
 function pog(){    
-    alert("hi");
+    
     if(document.getElementById("pass").value == ("buh")){
-    alert("succes")
+    
         showFiles();
     }
     else{

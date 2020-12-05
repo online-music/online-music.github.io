de4aecf3d00458aae85985b043b3b56030924ba4
blackdiamand <kevikeviwang@gmail.com>
2020-12-04 17:08:10 -0800

    fix

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var password = document.getElementById("password");
 var output = document.getElementById("output");
 
 password.oninput = function(event) {
+    console.log(password.value)
   if(password.value.equals("buh")){
       output.innerHTML = "pog";
   }
